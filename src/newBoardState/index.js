f4551c678d0c0cd0e62b05285aea8f2ceff1d926
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2017-09-07 15:11:54 -1000

    Sends one batch of files instead of forloop

--- src/newBoardState/index.js
+++ src/newBoardState/index.js
@@ -17,63 +17,33 @@ class newBoardController{
 	}
 
 	  uploadPic(file) {
-      console.log(file);
+      console.log("file",file);
       if(file && file.length){
-        for(var i = 0; i < file.length; i++){
-          file[i].upload = this.Upload.upload({
-            url: 'http://localhost:3000/api/newBoard',
-            method: 'POST',
-            data: {name: this.$scope.name, shaper: this.$scope.shaper, feet: this.$scope.feet, inches: this.$scope.inches, width: this.$scope.width, thickness: this.$scope.thickness, fins: this.$scope.fins, surfboardImg: {
-                name: file[i].name,
-                file: file[i]
-            }},
-          });
-
-          file[i].upload.then((response) => {
-            console.log(response);
-            this.$timeout(function () {
-              file[i].result = response.data;
-            });
-          }, (response) => {
-            console.log(response);
-            if (response.status > 0 || response.status == -1)
-              this.$scope.errorMsg = "Error message " + response.status + ': ' + response.data;
-          }, (evt) => {
-            // Math.min is to fix IE which reports 200% sometimes
-            this.$scope.file[i].progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-          })
-          .then(() => {
-            window.location.replace('/#!/test');
-          });
-        }
+        file.upload = this.Upload.upload({
+          url: 'http://localhost:3000/api/newBoard',
+          method: 'POST',
+          data: {name: this.$scope.name, shaper: this.$scope.shaper, feet: this.$scope.feet, inches: this.$scope.inches, width: this.$scope.width, thickness: this.$scope.thickness, fins: this.$scope.fins, surfboardImg: file, numOfFiles: file.length
+          }
+        });
+        window.location.replace('/#!/test');
       }
 
-
-      file[0].upload = this.Upload.upload({
-        url: 'http://localhost:3000/api/newBoard',
-        method: 'POST',
-        data: {name: this.$scope.name, shaper: this.$scope.shaper, feet: this.$scope.feet, inches: this.$scope.inches, width: this.$scope.width, thickness: this.$scope.thickness, fins: this.$scope.fins, surfboardImg: {
-        		name: file[0].name,
-        		file: file
-        }},
-      });
-
-      file[0].upload.then((response) => {
-      	console.log(response);
-        this.$timeout(function () {
-          file[0].result = response.data;
-        });
-      }, (response) => {
-      	console.log(response);
-        if (response.status > 0 || response.status == -1)
-          this.$scope.errorMsg = "Error message " + response.status + ': ' + response.data;
-      }, (evt) => {
-        // Math.min is to fix IE which reports 200% sometimes
-        this.$scope.file[0].progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-      })
-      .then(() => {
-      	window.location.replace('/#!/test');
-      });
+      // file[0].upload.then((response) => {
+      // 	console.log(response);
+      //   this.$timeout(function () {
+      //     file[0].result = response.data;
+      //   });
+      // }, (response) => {
+      // 	console.log(response);
+      //   if (response.status > 0 || response.status == -1)
+      //     this.$scope.errorMsg = "Error message " + response.status + ': ' + response.data;
+      // }, (evt) => {
+      //   // Math.min is to fix IE which reports 200% sometimes
+      //   this.$scope.file[0].progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+      // })
+      // .then(() => {
+      // 	window.location.replace('/#!/test');
+      // });
 
 
     }
